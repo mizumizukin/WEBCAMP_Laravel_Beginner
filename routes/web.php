37b6c8dbcea97b1017c26e18fc1528454f2124cb
MIZUKI NOJIMA <MIZUKI@example.com>
2024-08-24 12:39:37 +0000

    1Page目と2Page目を書いた

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\WelcomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/',[WelcomeController::class,'index']);
+Route::get('/welcome/second', [WelcomeController::class, 'second']);
